206e6e858bb722d5ae2bd7651bc4935af8ddf62a
TranHoangViet <viet14042000@gmail.com>
2021-05-24 17:13:05 +0700

    feat: setup firebase

--- Server/src/app.js
+++ Server/src/app.js
@@ -1,10 +1,17 @@
-import {} from 'dotenv/config';
+import { } from 'dotenv/config';
 import express from 'express';
+import admin from './firebase/firebase'
 
 const app = express();
 
-app.get('/api', (req, res) => {
-  res.send('Hello, things are good');
+app.get('/api', async (req, res) => {
+  try {
+    const response = await admin.firestore().collection('testing').doc('test').get();
+    res.status(200).send(response.data().content);
+  } catch (e) {
+    console.log(e)
+    res.status(500).send("Internal server error")
+  }
 });
 
 export default app;
--- Server/src/index.js
+++ Server/src/index.js
@@ -1,7 +1,6 @@
-import {} from 'dotenv/config';
+import { } from 'dotenv/config';
 import app from 'app';
-
-const { PORT } = process.env;
+const PORT = process.env.PORT || 4001
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
